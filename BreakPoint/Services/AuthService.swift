613dd96494fd493802df6fc2fd4c145e91f977b0
Mohammed Elnaggar <m.abdelwhab69@gmail.com>
2018-01-23 16:55:06 +0200

    first commit

---
+++ BreakPoint/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  BreakPoint
+//
+//  Created by Mohammed Elnaggar on 1/18/18.
+//  Copyright © 2018 Mohammed Elnaggar. All rights reserved.
+//
+
+import Foundation
---
+++ BreakPoint/View/InsertTextFiled.swift
@@ -0,0 +1,21 @@
+//
+//  InsertTextFiled.swift
+//  BreakPoint
+//
+//  Created by Mohammed Elnaggar on 1/18/18.
+//  Copyright © 2018 Mohammed Elnaggar. All rights reserved.
+//
+
+import UIKit
+
+class InsertTextFiled: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
